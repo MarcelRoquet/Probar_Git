af1a86ff19aaaf037dc733638428f359efde1648
Marcel Roquet <17marcelroquet@gmail.com>
2024-11-14 10:38:27 +0100

    comit desktop

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // esto sera la version inicial
+int versioinicial= 923;
+
+String V2="esra es la version 2 ";
+
+
+    }
+}
